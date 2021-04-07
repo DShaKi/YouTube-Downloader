46b2a056362732eacf9717188da44948ef6ba0a9
unknown <shayankermani1385@gmail.com>
2021-04-08 00:23:10 +0430

    Fix bugs and set message boxes

--- youtube-dl.py
+++ youtube-dl.py
@@ -1,9 +1,12 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QFormLayout, QLineEdit, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QFormLayout, QLineEdit, QPushButton, QLabel, QMessageBox
 from PyQt5.QtGui import QCursor, QIcon
 from PyQt5.QtCore import Qt, QThread
 import tkinter as tk
+import urllib
 from tkinter import messagebox
 from pytube import YouTube
+
+theard_error = 0 # 0: good, 1: error
 
 class download_thread(QThread):
     def __init__(self, input):
@@ -12,14 +15,29 @@ class download_thread(QThread):
 
     def run(self):
         url = self.input_btn.text()
-        youtube = YouTube(url)
-        video = youtube.streams.first()
-        root = tk.Tk()
-        root.withdraw()
-        messagebox.showinfo("Vidoe title", video.title)
-        messagebox.showinfo("Path", "Downloading in Downloads folder, please be patient!")
-        video.download('C:\Video')
-        messagebox.showinfo("Downloaded", "Downloaded!, Please check for the video in C:\Video")
+
+        try:
+            youtube = YouTube(url)
+        except RuntimeError:
+            self.show_error()
+        except urllib.error.URLError:
+            self.show_error()
+        else:
+            root = tk.Tk()
+            root.withdraw()
+            video = youtube.streams.first()
+            messagebox.showinfo("Vidoe title", video.title)
+            messagebox.showinfo("Path", "Downloading in Downloads folder, please be patient!")
+            video.download('C:\Video')
+            messagebox.showinfo("Downloaded", "Downloaded!, Please check for the video in C:\Video")
+
+    def show_error(self):
+        self.msg = QMessageBox()
+        self.msg.setIcon(QMessageBox.Critical)
+        self.msg.setWindowTitle("Unexcpected error")
+        self.msg.setText("Unexcpected error. Please try again.")
+
+        retval = self.msg.exec_()
 
 class Ui(QWidget):
     def __init__(self):
@@ -48,8 +66,19 @@ class Ui(QWidget):
         self.resize(1000, 120)
 
     def btn_clicked(self):
+        url = self.url_input.text()
+        root = tk.Tk()
+        root.withdraw()
+        if url == ' ':
+            messagebox.showerror("Null", "The input was null! please refill it and try again.")
+            return 0
+        elif not("https://www.youtube.com" in url):
+            messagebox.showerror("Incorrect", "You url is unvailable. It's incorrct actually.")
+            return 0
         self.download = download_thread(self.url_input)
         self.download.start()
+        if theard_error == 1:
+            messagebox.showerror("Unexcpected Error", "There was an unexcpected error.")
 
 if __name__ == "__main__":
     app = QApplication([])
